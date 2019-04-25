fd70be5c0a9d9c548b28ef418e159bb899a94f55
Kwon <finalsoul1@gmail.com>
2019-04-25 13:30:12 +0900

    Updates

---
+++ precache-manifest.65f05706db0020a9276004ded025695c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3641930c4874922b3706271f06f1b38f",
+    "url": "/git-page-test/index.html"
+  },
+  {
+    "revision": "80c1fe6beef020484249",
+    "url": "/git-page-test/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/git-page-test/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "80c1fe6beef020484249",
+    "url": "/git-page-test/static/js/main.b4e071f2.chunk.js"
+  },
+  {
+    "revision": "f43b318fccfb7ec92871",
+    "url": "/git-page-test/static/js/runtime~main.433b7de4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/git-page-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
